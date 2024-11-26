14115bdcdf516f6d6f8584b788a8131b2c9ca3c6
Firepaw2106 <therese.signeboen@hotmail.no>
2024-11-26 10:07:04 +0100

    trafikklys

--- Trafikklys/script.js
+++ Trafikklys/script.js
@@ -1,3 +1,24 @@
 
 
+function setYellowLight(){
 
+    activeLight('rød', 'grønn','gul')
+}
+
+function setGreenLight(){
+
+    activeLight('gul', 'rød', 'grønn'
+}
+
+function activeLight(divId1, divId2, divId3,divId4){
+
+    document.getElementById(divId1).classList.remove("active");
+    document.getElementById(divId2).classList.remove("active");
+    document.getElementById(divId3).classList.add("active");
+    document.getElementById(divId4).classList.add("active");
+    
+}
+function setRedLight(){
+    
+    activeLight('grønn','gul','rød','rød')
+}
