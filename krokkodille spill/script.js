195f0e80b54e66dfc0c0ea9fab16ecd6b029b1cd
Firepaw2106 <therese.signeboen@hotmail.no>
2024-11-28 13:22:26 +0100

    kokkodille

---
+++ krokkodille spill/script.js
@@ -0,0 +1,13 @@
+
+let TallNr1 =document.getElementById("Tallplass1").innerHTML.text
+let TallNr2 =document.getElementById("Tallplass2").innerHTML.text
+
+
+function RandomNub(){
+   TallNr1 = Math.floor(Math.random()*11)
+   TallNr2 = Math.floor(Math.random()*11)
+}
+
+function CheckAnswer(){
+
+}
