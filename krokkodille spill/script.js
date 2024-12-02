c27fa723577fe1f3775e9191990f9e8e6e753d26
Firepaw2106 <therese.signeboen@hotmail.no>
2024-12-02 14:44:47 +0100

    ytre

--- krokkodille spill/script.js
+++ krokkodille spill/script.js
@@ -1,17 +1,17 @@
 
-let TallNr1 = document.getElementById("Tallplass1").innerHTML;
-let TallNr2 = document.getElementById("Tallplass2").innerHTML;
-let sum = document.getElementById("Poengsum").innerHTML
-let svarInput = document.getElementById("input").innerHTML
+let TallNr1 = document.getElementById("Tallplass1");
+let TallNr2 = document.getElementById("Tallplass2");
+let sum = document.getElementById("Poengsum")
+let svarInput = document.getElementById("input")
 
 function RandomNub(){
-    document.getElementById("Tallplass1").innerHTML = Math.floor(Math.random() * 11);
-    document.getElementById("Tallplass2").innerHTML = Math.floor(Math.random()*11);
+    TallNr1.innerHTML = Math.floor(Math.random() * 11);
+    TallNr2.innerHTML = Math.floor(Math.random() * 11);
 }
 
 function CheckAnswer(){
 
-    if(TallNr1 < TallNr2 && svarInput = '<'){
+    if(TallNr1 <= TallNr2 && svarInput.){
         sum = +1
     }
 
