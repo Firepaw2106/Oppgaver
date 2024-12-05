47020d50bde024415866cd49d6dd8446833e6196
Firepaw2106 <therese.signeboen@hotmail.no>
2024-12-05 14:39:47 +0100

    fikk til korkodille spill begunte på adkjetiv his

---
+++ adjektiv historie/javascript/view.js
@@ -0,0 +1,12 @@
+
+const app = document.getElementById("app");
+
+function updateView(){
+ app.innerHTML = /*HTML*/`
+ <div>
+ <div>Lorem ipsum dolor sit amet consectetur</div><div>adjektiv </div>
+ <div> adipisicing elit. Voluptatibus eveniet expedita,
+  voluptatem accusantium impedit fuga libero nam magni, quaerat id omnis veniam officiis voluptas
+   excepturi. Ipsa dolor nulla voluptates ad!</div>
+   </div>`;
+}
--- krokkodille spill/script.js
+++ krokkodille spill/script.js
@@ -1,18 +1,44 @@
 
 let TallNr1 = document.getElementById("Tallplass1");
 let TallNr2 = document.getElementById("Tallplass2");
-let sum = document.getElementById("Poengsum")
-let svarInput = document.getElementById("input")
+let Poengsum = document.getElementById("Poengsum");
+let svarInput = document.getElementById("input");
+let sum = 0;
 
 function RandomNub(){
-    TallNr1.innerHTML = Math.floor(Math.random() * 11);
-    TallNr2.innerHTML = Math.floor(Math.random() * 11);
+    TallNr1.innerHTML = Math.floor(Math.random() * 10);
+    TallNr2.innerHTML = Math.floor(Math.random() * 10);
+    clearInput()
+
+    return TallNr1.innerHTML, TallNr2.innerHTML;
+    
 }
 
-function compareAnswer(){
 
-    if(TallNr1 < TallNr2 && svarInput.innerHTML = ("<"));{
-        sum = +1
+
+function compareAnswer(){
+    if(TallNr1.innerHTML < TallNr2.innerHTML && svarInput.value =='<'){
+        sum = sum +1
+        Poengsum.innerHTML =sum
+
+    }else if(TallNr1.innerHTML > TallNr2.innerHTML && svarInput.value =='>'){
+        sum = sum +1
+        Poengsum.innerHTML =sum
+
+    }else if(TallNr1.innerHTML == TallNr2.innerHTML && svarInput.value =='='){
+        sum = sum +1
+        Poengsum.innerHTML =sum
+
+    }else{
+        sum =sum -1
+        Poengsum.innerHTML =sum
     }
+    clearInput()
+    RandomNub()
+}
+
+function clearInput(){
+
+svarInput.value="";
 
 }
