ea77da750c7cf68c7b0758d525459e2c6d7b96ea
Firepaw2106 <therese.signeboen@hotmail.no>
2024-12-09 09:15:17 +0100

    tr

--- adjektiv historie/javascript/view.js
+++ adjektiv historie/javascript/view.js
@@ -4,7 +4,8 @@ const app = document.getElementById("app");
 function updateView(){
  app.innerHTML = /*HTML*/`
  <div>
- <div>Lorem ipsum dolor sit amet consectetur</div><div>adjektiv </div>
+ <div>Lorem ipsum dolor sit amet consectetur</div>
+ <div>adjektiv </div>
  <div> adipisicing elit. Voluptatibus eveniet expedita,
   voluptatem accusantium impedit fuga libero nam magni, quaerat id omnis veniam officiis voluptas
    excepturi. Ipsa dolor nulla voluptates ad!</div>
